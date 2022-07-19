263ef8389f53c63971a0410a49611430c9ecd8b6
Tommy <tommy128works@gmail.com>
2022-07-18 20:51:44 -0400

    Create operate function

--- js-file.js
+++ js-file.js
@@ -12,7 +12,17 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-  };
+};
 
-
-
+function operate(a, x, b) {
+    switch (x) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
