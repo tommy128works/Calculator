f2cda65c539807905a1942999a302138637002c0
Tommy <tommy128works@gmail.com>
2023-05-11 16:21:01 -0400

    chore: set up files for new project
    Set up HTML, CSS, JavaScript, ESLint, and Prettier for new project.

--- js-file.js
+++ js-file.js
@@ -1,111 +1,107 @@
 function add(a, b) {
-	return a + b;
-};
+  return a + b;
+}
 
 function subtract(a, b) {
-	return a - b;
-};
+  return a - b;
+}
 
 function multiply(a, b) {
   return a * b;
-};
-
-function divide(a, b) {
-    return a / b;
-};
-
-function operate(a, x, b) {
-    a = parseInt(a);
-    b = parseInt(b);
-    switch (x) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
-    }
 }
 
-let display = '0';
-let operator = '';
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, x, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  switch (x) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+let display = "0";
+let operator = "";
 let firstNum = 0;
 let startSecondNum = false;
 
-const displayQuery = document.querySelector('#display');
+const displayQuery = document.querySelector("#display");
 function updateDisplay() {
-    displayQuery.textContent = display;
+  displayQuery.textContent = display;
 }
 
 for (let i = 0; i < 10; i++) {
-    let numTemp = document.querySelector('button[value="' +i+ '"]');
-    numTemp.addEventListener('click', () => {
-        if (display === '0' || (operator != '' && startSecondNum === true)) {
-            display = String(i);
-            startSecondNum = false;
-        }
-        else {
-            display += i;
-        }
-        updateDisplay();
-    })
+  let numTemp = document.querySelector('button[value="' + i + '"]');
+  numTemp.addEventListener("click", () => {
+    if (display === "0" || (operator != "" && startSecondNum === true)) {
+      display = String(i);
+      startSecondNum = false;
+    } else {
+      display += i;
+    }
+    updateDisplay();
+  });
 }
 
 const addButton = document.querySelector('button[value="+"]');
-addButton.addEventListener('click', () => {
-    if (operator === '') {
-        operator = '+';
-        firstNum = parseInt(display);
-        startSecondNum = true;
-    }
-})
+addButton.addEventListener("click", () => {
+  if (operator === "") {
+    operator = "+";
+    firstNum = parseInt(display);
+    startSecondNum = true;
+  }
+});
 
 const subtractButton = document.querySelector('button[value="-"]');
-subtractButton.addEventListener('click', () => {
-    if (operator === '') {
-        operator = '-';
-        firstNum = parseInt(display);
-        startSecondNum = true;
-    }
-})
+subtractButton.addEventListener("click", () => {
+  if (operator === "") {
+    operator = "-";
+    firstNum = parseInt(display);
+    startSecondNum = true;
+  }
+});
 
 const multiplyButton = document.querySelector('button[value="*"]');
-multiplyButton.addEventListener('click', () => {
-    if (operator === '') {
-        operator = '*';
-        firstNum = parseInt(display);
-        startSecondNum = true;
-    }
-})
+multiplyButton.addEventListener("click", () => {
+  if (operator === "") {
+    operator = "*";
+    firstNum = parseInt(display);
+    startSecondNum = true;
+  }
+});
 
 const divideButton = document.querySelector('button[value="/"]');
-divideButton.addEventListener('click', () => {
-    if (operator === '') {
-        operator = '/';
-        firstNum = parseInt(display);
-        startSecondNum = true;
-    }
-})
-
+divideButton.addEventListener("click", () => {
+  if (operator === "") {
+    operator = "/";
+    firstNum = parseInt(display);
+    startSecondNum = true;
+  }
+});
 
 const equalButton = document.querySelector('button[value="="]');
-equalButton.addEventListener('click', () => {
-    if (firstNum != 0) {
-        display = operate(firstNum, operator, display);
-        firstNum = 0;
-        operator = '';
-    }
-    updateDisplay();
-})
-
+equalButton.addEventListener("click", () => {
+  if (firstNum != 0) {
+    display = operate(firstNum, operator, display);
+    firstNum = 0;
+    operator = "";
+  }
+  updateDisplay();
+});
 
 const clearButton = document.querySelector('button[value="clear"]');
-clearButton.addEventListener('click', () => {
-    display = '0';
-    firstNum = 0;
-    operator = '';
-    updateDisplay();
-})
-
+clearButton.addEventListener("click", () => {
+  display = "0";
+  firstNum = 0;
+  operator = "";
+  updateDisplay();
+});
